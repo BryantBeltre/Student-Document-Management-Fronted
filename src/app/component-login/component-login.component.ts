d6f809c96515469a4ec53a91d27811d9c4b9bbf2
Francis Beltre <fbeltre@sns.gob.do>
2024-08-12 12:53:58 -0400

    Initial Commit

---
+++ src/app/component-login/component-login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './component-login.component.html',
+  styleUrl: './component-login.component.css'
+})
+export class ComponentLoginComponent {
+
+}
